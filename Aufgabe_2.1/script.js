7730475e42df9cfa2b4a0c35134bbb6bc6ea929a
Dobsonstudio <80950322+Dobsonstudio@users.noreply.github.com>
2021-04-23 12:32:56 +0200

    TS+JS Beginning

---
+++ Aufgabe_2.1/script.js
@@ -0,0 +1,23 @@
+"use strict";
+console.log("Hallo Welt");
+// var bEZEICHNUNG: Typ = Wert;
+// console.log(true + true);
+console.log(5 + 1);
+console.log(5 + "1");
+console.log("5" + 1);
+// console.log('5' - 1);
+//BONUS:
+console.log(("b" + "a" + +"a" + "a").toLowerCase());
+console.log(`#`);
+console.log(`##`);
+console.log(`###`);
+console.log(`####`);
+console.log(`#####`);
+let x = 10;
+let five = 5;
+console.log(five * five);
+let weather = "sunny";
+console.log(weather);
+weather = "dark";
+console.log(weather);
+//# sourceMappingURL=script.js.map
---
+++ Aufgabe_2.1/script.ts
@@ -0,0 +1,27 @@
+console.log("Hallo Welt");
+// var bEZEICHNUNG: Typ = Wert;
+
+// console.log(true + true);
+console.log(5 + 1);
+console.log(5 + "1");
+console.log("5" + 1);
+// console.log('5' - 1);
+
+//BONUS:
+console.log(("b" + "a" + + "a" + "a").toLowerCase());
+
+console.log(`#`);
+console.log(`##`);
+console.log(`###`);
+console.log(`####`);
+console.log(`#####`);
+
+let x: number = 10;
+
+let five: number = 5;
+console.log(five * five);
+
+let weather: string = "sunny";
+console.log(weather);
+weather = "dark";
+console.log(weather);
