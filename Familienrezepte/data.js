cbe0b56e518dc05f4be826bd5e15e499a15c0711
Dobsonstudio <80950322+Dobsonstudio@users.noreply.github.com>
2021-07-09 14:23:39 +0200

    changes

--- Familienrezepte/data.js
+++ Familienrezepte/data.js
@@ -3,11 +3,23 @@ var Rezepte_Server;
 (function (Rezepte_Server) {
     let showresponse = document.getElementById("response");
     document.getElementById("addToDB").addEventListener("click", addToDB);
+    document.getElementById("login").addEventListener("click", login);
     async function addToDB() {
         let formData = new FormData(document.forms[0]);
         let url = "https://dobsonstudio2021.herokuapp.com";
         let query = new URLSearchParams(formData);
         url += "/addDB";
+        url += "?" + query.toString();
+        let response = await fetch(url, { method: "get" });
+        let responseText = await response.text();
+        console.log(responseText);
+        showResponseFunc(responseText);
+    }
+    async function login() {
+        let formData = new FormData(document.forms[0]);
+        let url = "https://dobsonstudio2021.herokuapp.com";
+        let query = new URLSearchParams(formData);
+        url += "/login";
         url += "?" + query.toString();
         let response = await fetch(url, { method: "get" });
         let responseText = await response.text();
--- Familienrezepte/data.ts
+++ Familienrezepte/data.ts
@@ -2,6 +2,7 @@ namespace Rezepte_Server {
     
     let showresponse: HTMLDivElement = <HTMLDivElement>document.getElementById("response");
     document.getElementById("addToDB").addEventListener("click", addToDB);
+    document.getElementById("login").addEventListener("click", login);
     
     async function addToDB(): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]);
@@ -15,6 +16,19 @@ namespace Rezepte_Server {
         console.log(responseText);
         showResponseFunc(responseText);
         }    
+
+    async function login(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        let url: RequestInfo = "https://dobsonstudio2021.herokuapp.com";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url += "/login";
+        url += "?" + query.toString();  
+        let response: Response = await fetch(url, { method: "get"});
+        let responseText: string = await response.text();
+                    
+        console.log(responseText);
+        showResponseFunc(responseText);
+        }  
 
     function showResponseFunc(text: string): void {
         showresponse.innerHTML = text;
