bac6f5ad6b52d4e9d33e9ff55bd1d36530507c5b
Dobsonstudio <80950322+Dobsonstudio@users.noreply.github.com>
2021-07-10 14:42:02 +0200

    loginjs

---
+++ Familienrezepte/login.js
@@ -0,0 +1,21 @@
+"use strict";
+var Rezepte_Server;
+(function (Rezepte_Server) {
+    let showresponse = document.getElementById("response");
+    document.getElementById("buttonLogin").addEventListener("click", login);
+    async function login() {
+        let formData = new FormData(document.forms[0]);
+        let url = "https://dobsonstudio2021.herokuapp.com";
+        let query = new URLSearchParams(formData);
+        url += "/login";
+        url += "?" + query.toString();
+        let response = await fetch(url, { method: "get" });
+        let responseText = await response.text();
+        console.log(responseText);
+        showResponseFunc(responseText);
+    }
+    function showResponseFunc(text) {
+        showresponse.innerHTML = text;
+    }
+})(Rezepte_Server || (Rezepte_Server = {}));
+//# sourceMappingURL=login.js.map
---
+++ Familienrezepte/login.ts
@@ -0,0 +1,27 @@
+namespace Rezepte_Server {
+    let showresponse: HTMLDivElement = <HTMLDivElement>document.getElementById("response");
+    document.getElementById("buttonLogin").addEventListener("click", login);
+   
+    async function login(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        let url: RequestInfo = "https://dobsonstudio2021.herokuapp.com";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url += "/login";
+        url += "?" + query.toString();  
+        let response: Response = await fetch(url, { method: "get"});
+        let responseText: string = await response.text();
+
+        console.log(responseText);
+        showResponseFunc(responseText);
+        }
+    
+
+    function showResponseFunc(text: string): void {
+        showresponse.innerHTML = text;
+    }
+
+    export interface FormElements {
+        username: string;
+        password: string;
+    }
+}
