079cf8d89d328359270a944bf60a92ffd192b6e1
Dobsonstudio <80950322+Dobsonstudio@users.noreply.github.com>
2021-07-11 15:14:27 +0200

    cleared out data.js

--- Familienrezepte/data.js
+++ Familienrezepte/data.js
@@ -3,7 +3,6 @@ var Rezepte_Server;
 (function (Rezepte_Server) {
     let showresponse = document.getElementById("response");
     document.getElementById("addToDB").addEventListener("click", addToDB);
-    document.getElementById("showAllRecipes").addEventListener("click", showAllRecipes);
     async function addToDB() {
         let formData = new FormData(document.forms[0]);
         let url = "https://dobsonstudio2021.herokuapp.com";
@@ -17,27 +16,6 @@ var Rezepte_Server;
     }
     function showResponseFunc(text) {
         showresponse.innerHTML = text;
-    }
-    async function showAllRecipes() {
-        let formData = new FormData(document.forms[0]);
-        let url = "https://dobsonstudio2021.herokuapp.com";
-        let query = new URLSearchParams(formData);
-        url += "/showAllRecipes";
-        url += "?" + query.toString();
-        let response = await fetch(url, { method: "get" });
-        let responseText = await response.text();
-        console.log("Retrieved JSON", JSON.parse(responseText));
-        let responseJSON = JSON.parse(responseText);
-        for (let i = 0; i < responseJSON.length; i++) {
-            let temp = document.createElement("div");
-            temp.className = "databaseEntry";
-            temp.innerHTML =
-                "<br> Autor: " + responseJSON[i].authorName +
-                    "<br> Rezept: " + responseJSON[i].rezeptName +
-                    "<br> Zutaten: " + responseJSON[i].ingredient +
-                    "<br> Zubereitung: " + responseJSON[i].zubereitung + "<br>";
-            showresponse.appendChild(temp);
-        }
     }
 })(Rezepte_Server || (Rezepte_Server = {}));
 //# sourceMappingURL=data.js.map
--- Familienrezepte/data.ts
+++ Familienrezepte/data.ts
@@ -1,7 +1,6 @@
 namespace Rezepte_Server {
     let showresponse: HTMLDivElement = <HTMLDivElement>document.getElementById("response");
     document.getElementById("addToDB").addEventListener("click", addToDB);
-    document.getElementById("showAllRecipes").addEventListener("click", showAllRecipes);
     
     async function addToDB(): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]);
@@ -20,49 +19,14 @@ namespace Rezepte_Server {
         showresponse.innerHTML = text;
         }
 
-    async function showAllRecipes(): Promise<void> {
-        let formData: FormData = new FormData(document.forms[0]);
-        let url: RequestInfo = "https://dobsonstudio2021.herokuapp.com";
-        let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url += "/showAllRecipes";
-        url += "?" + query.toString();  
-        let response: Response = await fetch(url, { method: "get"});
-        let responseText: string = await response.text();
-
-        console.log("Retrieved JSON", JSON.parse(responseText));
-        let responseJSON: RecipeData[] = JSON.parse(responseText);
-    
-        for (let i: number = 0; i < responseJSON.length; i++) {
-        let temp: HTMLDivElement = <HTMLDivElement>document.createElement("div");
-        
-        temp.className = "databaseEntry";
-        temp.innerHTML =  
-        "<br> Autor: " + responseJSON[i].authorName +
-        "<br> Rezept: " + responseJSON[i].rezeptName +
-        "<br> Zutaten: " + responseJSON[i].ingredient + 
-        "<br> Zubereitung: " + responseJSON[i].zubereitung + "<br>";
-        showresponse.appendChild(temp);
-        }
-
-        interface FormElements {
+    interface FormElements {
             username: string;
             password: string;
         }
 
-        interface NewRecipe {
-            authorName: string;
-            rezeptName: string;
-            ingredient: string[];
-            zubereitung: string;
-        }
-
-        interface CollectionData extends FormElements {
+    interface CollectionData extends FormElements {
             _id: string;
         }
 
-        interface RecipeData extends NewRecipe {
-            _id: string;
-        }
     }
     
-}
