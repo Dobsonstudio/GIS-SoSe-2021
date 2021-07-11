fc7b2107aefcf8668e877f97339c55dd780c56dc
Dobsonstudio <80950322+Dobsonstudio@users.noreply.github.com>
2021-07-11 17:15:53 +0200

    recipe

--- Familienrezepte/recipes.js
+++ Familienrezepte/recipes.js
@@ -2,7 +2,23 @@
 var Rezepte_Server;
 (function (Rezepte_Server) {
     let showresponse = document.getElementById("response");
+    document.getElementById("addNewRecipe").addEventListener("click", addRecipe);
     document.getElementById("showAllRecipes").addEventListener("click", showAllRecipes);
+    async function addRecipe() {
+        let formData = new FormData(document.forms[0]);
+        let url = "https://dobsonstudio2021.herokuapp.com";
+        //let url: RequestInfo = "http://localhost:8100";
+        let query = new URLSearchParams(formData);
+        url += "/addNewRecipe";
+        url += "?" + query.toString() + "&authorName=" + localStorage.getItem("username");
+        let response = await fetch(url, { method: "get" });
+        let responseText = await response.text();
+        console.log(responseText);
+        showResponseFunc(responseText);
+    }
+    function showResponseFunc(text) {
+        showresponse.innerHTML = text;
+    }
     async function showAllRecipes() {
         let formData = new FormData(document.forms[0]);
         let url = "https://dobsonstudio2021.herokuapp.com";
@@ -26,4 +42,4 @@ var Rezepte_Server;
         }
     }
 })(Rezepte_Server || (Rezepte_Server = {}));
-//# sourceMappingURL=showAllRecipes.js.map
+//# sourceMappingURL=recipes.js.map
--- Familienrezepte/recipes.ts
+++ Familienrezepte/recipes.ts
@@ -1,6 +1,25 @@
 namespace Rezepte_Server {
     let showresponse: HTMLDivElement = <HTMLDivElement>document.getElementById("response");
+    document.getElementById("addNewRecipe").addEventListener("click", addRecipe);
     document.getElementById("showAllRecipes").addEventListener("click", showAllRecipes);
+
+    async function addRecipe(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        let url: RequestInfo = "https://dobsonstudio2021.herokuapp.com";
+        //let url: RequestInfo = "http://localhost:8100";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url += "/addNewRecipe";
+        url += "?" + query.toString() + "&authorName=" + localStorage.getItem("username");  
+        let response: Response = await fetch(url, { method: "get"});
+        let responseText: string = await response.text();
+            
+        console.log(responseText);
+        showResponseFunc(responseText);
+        }    
+
+    function showResponseFunc(text: string): void {
+        showresponse.innerHTML = text;
+    }
 
     async function showAllRecipes(): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]);
@@ -38,5 +57,4 @@ namespace Rezepte_Server {
             _id: string;
         }
     }
-    
 }
